a1c2ba1dd0c4a88c4863b03d0ad868bb624f0ea8
opmmansoor <opmmansoor@gmail.com>
2026-07-02 21:14:10 +0530

    Weather - Favourite Button added

--- weather.js
+++ weather.js
@@ -2,9 +2,11 @@ const API_KEY = "b53f3f448d82edfcd171b747c8f062df";
 const URL = "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric"
 
 
-let searchBox = document.getElementById("search-input")
-let searchBtn = document.querySelector(".btn")
-
+let searchBox = document.getElementById("search-input");
+let searchBtn = document.querySelector(".btn");
+//let favourites = document.getElementsByClassName("favorateBtn");
+const favoriteBtn = document.querySelector(".favorateBtn");
+let favouriteList = document.querySelector(".fav-list")
 
 
 // Fetch API
@@ -13,12 +15,13 @@ async function checkWeather(city = `kerala`) {
  let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`) 
  let data = await response.json();
 
-    document.querySelector(".city").innerHTML  = data.name
+    document.querySelector(".city").innerHTML = data.name
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+"°c";
+    document.querySelector(".Description").innerHTML = data.weather[0].description;
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".feelslike").innerHTML = Math.round(data.main.feels_like) + "°c";
     document.querySelector(".wind").innerHTML  = data.wind.speed + "Km/h";
 }
-
 
 //Search button
 searchBtn.addEventListener("click",() =>{
@@ -31,3 +34,65 @@ searchBox.addEventListener("keyup",(e) => {
     }
 })
 checkWeather();
+
+
+//add City to favourite
+
+function addFavourite(city) {
+    let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+    if (!favorites.includes(city)) {
+        favorites.push(city);
+
+        localStorage.setItem(
+            "favorites",
+            JSON.stringify(favorites)
+        );
+
+        loadFavorites(); 
+    }
+}
+
+//Load Favourites
+function loadFavorites() {
+    let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+    favouriteList.innerHTML = "";
+
+    favorites.forEach(city => {
+        favouriteList.innerHTML += `
+            <div class="favorite-item">
+                ${city}
+                 <button class="fav-delBtn">
+                <img src="del.png" alt="">
+             </button>
+            </div>
+        `;
+    });
+}
+
+loadFavorites();
+
+favoriteBtn.addEventListener("click", () => {
+    addFavourite(document.querySelector(".city").innerText);
+});
+
+favoriteBtn.addEventListener("click", () => {
+    console.log("clicked");
+});
+
+//Delete favorite city
+function removeFavorite(city) {
+    let favorites = JSON.parse(
+        localStorage.getItem("favorites")
+    ) || [];
+
+    favorites = favorites.filter(
+        fav => fav !== city
+    );
+
+    localStorage.setItem(
+        "favorites",
+        JSON.stringify(favorites)
+    );
+}
